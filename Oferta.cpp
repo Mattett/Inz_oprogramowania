5d04043611c5b92cffb4964982cefb21e08b9ab2
Mattett <43877902+Mattett@users.noreply.github.com>
2018-11-21 12:37:57 +0100

    Add files via upload

---
+++ Oferta.cpp
@@ -0,0 +1,37 @@
+#include "Oferta.h"
+
+
+
+Oferta::Oferta()
+{
+	
+		++ID_oferty;
+		koszt_oferty = 0;
+		czas_trwania_oferty = 0;
+		opis_oferty = "brak";
+
+}
+
+Oferta::Oferta(double price, double time, string description)
+{
+	++ID_oferty;
+	koszt_oferty = price;
+	czas_trwania_oferty = time;
+	opis_oferty = description;
+}
+
+Oferta::~Oferta()
+{
+}
+
+ostream & operator<<(ostream& str, Oferta & o)
+
+{
+
+	str << "ID to: " <<o.ID_oferty << endl << "Koszt oferty:  " << o.koszt_oferty << endl << "Czas trwania oferty:  "
+		<< o.czas_trwania_oferty << endl << "Opis oferty: " << o.opis_oferty << endl;
+
+
+	return str;
+
+}
---
+++ Oferta.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "MainHeader.h"
+
+class Oferta
+{
+protected:
+	 static int ID_oferty;
+	double koszt_oferty;
+	double czas_trwania_oferty;
+	string opis_oferty;
+
+public:
+	Oferta();
+	Oferta(double, double, string);
+	~Oferta();
+
+	friend ostream & operator<<(ostream&, Oferta &);
+
+};
+
